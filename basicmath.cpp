1154f369a05d52560546d7a0a7e74717f5b47193
Angel Irangabiye <angel.irangabiyr@stu.ocsb.ca>
2025-02-20 07:43:38 +0000

    Added basicmath.cpp

---
+++ basicmath.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2025 Angel All rights reserved
+//
+// Created by: Angel
+// Created on Feb 17,2025
+// This program does basic math
+
+#include <iostream>
+
+int main() {
+    std::cout << "2+3=" << (2+3) << std::endl;
+    std::cout << "5-4=" << (5-4) << std::endl;
+    std::cout << "8*6=" << (8*6) << std::endl;
+    std::cout << "8/3=" << (8/3) << std::endl;
+    std::cout << "3^2=" << (3^2) << std::endl;
+    std::cout << "3+4*2=" << (3+4*2) << std::endl;
+}
